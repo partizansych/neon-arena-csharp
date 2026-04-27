8f71b74f1d6b8c443f60e56a7288a71e5bd50513
Maxim Chaptykov <maks.chaptykov@gmail.com>
2026-04-28 06:52:27 +0700

    feat: добавить сигнал смерти врагу

--- Enemy.cs
+++ Enemy.cs
@@ -2,12 +2,20 @@ using Godot;
 
 public partial class Enemy : CharacterBody2D
 {
+    [Signal] public delegate void DiedEventHandler();
+
     [Export] public float Hp = 100f;
 
     public void TakeDamage(float amount)
     {
         Hp -= amount;
         if (Hp <= 0f)
-            QueueFree();
+            Die();
+    }
+
+    public void Die()
+    {
+        QueueFree();
+        EmitSignal(SignalName.Died);
     }
 }

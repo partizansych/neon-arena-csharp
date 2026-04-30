a09ea287bb4103664c6088f7f997f63e7faa9d0f
Maxim Chaptykov <maks.chaptykov@gmail.com>
2026-05-01 05:33:14 +0700

    use stat component in player

--- Player.cs
+++ Player.cs
@@ -3,24 +3,24 @@ using NeonArenaCsharp;
 
 [GlobalClass]
 public partial class Player : CharacterBody2D {
-    [Export] public float Speed = 200f;
-    [Export] public WeaponController WeaponController;
+    [Export] StatsContainer statsContainer;
+    [Export] WeaponController weaponController;
 
     public override void _PhysicsProcess(double delta) {
         var direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
-        Velocity = direction * Speed;
+        Velocity = direction * statsContainer.Speed.Value;
         MoveAndSlide();
 
         if (Input.IsActionPressed("attack")) {
-            WeaponController.IsShooting = true;
-            WeaponController.ShootDirection = GlobalPosition.DirectionTo(GetGlobalMousePosition());
+            weaponController.IsShooting = true;
+            weaponController.ShootDirection = GlobalPosition.DirectionTo(GetGlobalMousePosition());
         }
         else {
-            WeaponController.IsShooting = false;
+            weaponController.IsShooting = false;
         }
 
         if (Input.IsActionJustPressed("reload")) {
-            WeaponController.Reload();
+            weaponController.Reload();
         }
     }
 }

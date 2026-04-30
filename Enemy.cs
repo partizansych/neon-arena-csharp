f6a360fe92568a1b96df9a1577cb87c107983370
Maxim Chaptykov <maks.chaptykov@gmail.com>
2026-05-01 01:09:23 +0700

    add enemy hit sound

--- Enemy.cs
+++ Enemy.cs
@@ -6,6 +6,7 @@ public partial class Enemy : CharacterBody2D, IDamageable {
     [Signal] public delegate void DiedEventHandler();
 
     [ExportGroup("Ассеты")]
+    [Export] AudioStreamWav HitSound;
     [Export] AudioStreamWav DeathSound;
 
     [ExportGroup("Компоненты")]
@@ -13,11 +14,16 @@ public partial class Enemy : CharacterBody2D, IDamageable {
     [Export] Health health;
 
     public override void _Ready() {
-        health.Died += () => EmitSignal(SignalName.Died);
+        health.Died += OnDied;
     }
 
     public void TakeDamage(float amount) {
         health.Current -= amount;
+        Audio.Instance.Play(HitSound);
+    }
+
+    private void OnDied() {
         Audio.Instance.Play(DeathSound);
+        EmitSignal(SignalName.Died);
     }
 }

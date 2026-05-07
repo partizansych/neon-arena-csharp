46293cc01ed66ab6a651a8c0b5154b7b7acd219a
Maxim Chaptykov <maks.chaptykov@gmail.com>
2026-05-07 13:36:33 +0700

    combat-pipeline

--- Combat.cs
+++ Combat.cs
@@ -1,15 +1,52 @@
+using System.Collections.Generic;
 using Godot;
 
 namespace NeonArenaCsharp;
 
-public struct Request {
-    public Character Source;
-    public Character Target;
+public struct DamageContext {
+    public Node Source;
+    public Node Target;
     public float Damage;
+    public bool IsCritical;
+}
+
+public interface IDamageStep {
+    void Execute(DamageContext context);
+}
+
+public class CriticalStrikeStep : IDamageStep {
+    public void Execute(DamageContext context) {
+        if (GD.Randf() < 0.15f) {
+            context.Damage *= 1.5f;
+            context.IsCritical = true;
+        }
+    }
+}
+
+public class ApplyDamageStep : IDamageStep {
+    public void Execute(DamageContext context) {
+        if (context.Target is IDamageable damageable) {
+            damageable.TakeDamage(context.Damage);
+        }
+    }
 }
 
 public partial class Combat : Node {
-    public void Request(Request request) {
-        request.Target.TakeDamage(request.Damage);
+    public static Combat Instance { get; private set; }
+
+    readonly List<IDamageStep> pipeline = [];
+
+    public override void _Ready() {
+        Instance = this;
+
+        pipeline.Add(new CriticalStrikeStep());
+        pipeline.Add(new ApplyDamageStep());
+    }
+
+    public void Request(DamageContext context) {
+        var target = context.Target;
+        if (target != null && IsInstanceValid(target))
+            foreach (var step in pipeline)
+                step.Execute(context);
     }
 }

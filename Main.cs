bd599c6d64d12c288a7f71b1a1fb9d25ebbb0c8f
Maxim Chaptykov <maks.chaptykov@gmail.com>
2026-05-22 04:48:45 +0700

    Добавить пушку игроку

--- Main.cs
+++ Main.cs
@@ -3,6 +3,7 @@ using Godot;
 [GlobalClass]
 public partial class Main : Node2D {
     [ExportGroup("Первоначальные оружия")]
+    [Export] PackedScene gunScene;
     [Export] GunData primary;
     [Export] GunData heavy;
 
@@ -11,12 +12,20 @@ public partial class Main : Node2D {
     [Export] PlayerData playerData;
 
     public override void _Ready() {
-        SpawnPlayer();
+        var player = CreatePlayer();
+        player.EquipGun(CreateGun());
     }
 
-    private void SpawnPlayer() {
+    private Player CreatePlayer() {
         var player = playerScene.Instantiate<Player>();
         player.Setup(playerData);
         AddChild(player);
+        return player;
+    }
+
+    private Gun CreateGun() {
+        var gun = gunScene.Instantiate<Gun>();
+        gun.Setup(primary);
+        return gun;
     }
 }
--- Player.cs
+++ Player.cs
@@ -6,10 +6,24 @@ public partial class Player : CharacterBody2D, IDamageable {
     [Export] Health health;
 
     PlayerData data;
+    Gun gun;
+
+    public override void _Process(double delta) {
+        if (gun != null && Input.IsActionPressed("attack")) {
+            var mousePos = GetGlobalMousePosition();
+            var direction = GlobalPosition.DirectionTo(mousePos);
+            gun.DoShot(direction);
+        }
+    }
 
     public void Setup(PlayerData data) {
         this.data = data;
         stats.Setup(data);
+    }
+
+    public void EquipGun(Gun gun) {
+        this.gun = gun;
+        AddChild(gun);
     }
 
     public void TakeDamage(float amount) {

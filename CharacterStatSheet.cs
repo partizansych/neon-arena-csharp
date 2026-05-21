a87930a1e4a42ab878f67cc02c06359c79f82496
Maxim Chaptykov <maks.chaptykov@gmail.com>
2026-05-22 04:06:26 +0700

    массовое удаление

--- CharacterStatSheet.cs
+++
@@ -1,8 +0,0 @@
-using NeonArenaCsharp;
-
-public class CharacterStatSheet : StatSheet<StatType> {
-    public CharacterStatSheet(CharacterData data) {
-        Register(StatType.Speed, data.Speed);
-        Register(StatType.MaxHp, data.MaxHp);
-    }
-}

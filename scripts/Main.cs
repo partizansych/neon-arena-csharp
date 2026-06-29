3e0960f80876d150fbeb8b581077cb5df459dc4c
Maxim Chaptykov <maks.chaptykov@gmail.com>
2026-06-30 01:12:43 +0700

    добавить паузу

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -4,12 +4,43 @@ using Godot;
 public partial class Main : Node {
     [Export] Node2D worldRoot;
     [Export] Control hudRoot;
+    [Export] Control menuRoot;
 
     const string ArenaLevelUID = "uid://cn8csvm0p4o6d";
+    const string PauseMenuUID = "uid://yfp0fhs7qda7";
+
+    Arena arena;
+    Control pauseMenu;
 
     public override void _Ready() {
+        PlaceArena();
+        PlacePauseMenu();
+    }
+
+    public override void _Process(double delta) {
+        if (Input.IsActionJustPressed("ui_cancel")) {
+            if (pauseMenu.Visible) SetPauseMenuState(isVisible: false);
+            else SetPauseMenuState(isVisible: true);
+        }
+    }
+
+    private void PlaceArena() {
         var arenaPacked = ResourceLoader.Load<PackedScene>(ArenaLevelUID);
-        var arena = arenaPacked.Instantiate();
+        arena = arenaPacked.Instantiate<Arena>();
         worldRoot.AddChild(arena);
+    }
+
+    private void PlacePauseMenu() {
+        var menuPacked = ResourceLoader.Load<PackedScene>(PauseMenuUID);
+        pauseMenu = menuPacked.Instantiate<Control>();
+        menuRoot.AddChild(pauseMenu);
+
+        SetPauseMenuState(isVisible: false);
+    }
+
+    private void SetPauseMenuState(bool isVisible) {
+        if (isVisible) pauseMenu.Show();
+        else pauseMenu.Hide();
+        pauseMenu.GetTree().Paused = isVisible;
     }
 }

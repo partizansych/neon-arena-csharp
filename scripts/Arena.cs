50492c087307dcb34d152a7cb84eeb496c5e45dd
Maxim Chaptykov <maks.chaptykov@gmail.com>
2026-06-29 07:42:16 +0700

    изменения Arena: - точка входа в уровень - стал ответственным за создание игрока - замена WaveManager -> ProtostarSpawner

---
+++ scripts/Arena.cs
@@ -0,0 +1,24 @@
+using Godot;
+
+[GlobalClass]
+public partial class Arena : Node2D {
+    [Export] ProtostarSpawner protostarSpawner;
+    [Export] Marker2D playerSpawnpoint;
+
+    const string PlayerUID = "uid://dwwpawwnocksd";
+
+    Player player;
+
+    public override void _Ready() {
+        PlacePlayer();
+
+        protostarSpawner.Target = player;
+        protostarSpawner.Container = this;
+    }
+
+    private void PlacePlayer() {
+        var playerPacked = ResourceLoader.Load<PackedScene>(PlayerUID);
+        player = playerPacked.Instantiate<Player>();
+        AddChild(player);
+    }
+}

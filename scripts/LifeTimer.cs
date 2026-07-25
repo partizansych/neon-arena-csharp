342d0fadeb2b58b7591fbb7a3e990ea449388d87
Maxim Chaptykov <maks.chaptykov@gmail.com>
2026-07-25 17:18:01 +0700

    add: LifeTimer component

---
+++ scripts/LifeTimer.cs
@@ -0,0 +1,22 @@
+using Godot;
+
+[GlobalClass]
+public partial class LifeTimer : Node {
+    [Export] public Node Target { get; set;}
+    [Export] public float Lifetime { get; set; } = 2.0f;
+    [Export] public bool AutoStart { get; set; } = true;
+
+    public override void _Ready() {
+        Target ??= GetParent<Node>();
+        if (AutoStart) Start(Lifetime);
+    }
+
+    public void Start(float time) {
+        if (time <= 0) return;
+        GetTree().CreateTimer(time).Timeout += OnTimeout;
+    }
+
+    private void OnTimeout() {
+        Target.QueueFree();
+    }
+}

d9e9c4b1e56fa3de8edc96ee440dbff071ad7dfa
Maxim Chaptykov <maks.chaptykov@gmail.com>
2026-08-08 06:00:54 +0700

    fix(Lifetimer): free when paused

--- scripts/Lifetimer.cs
+++ scripts/Lifetimer.cs
@@ -6,14 +6,25 @@ public partial class Lifetimer : Node {
     [Export] public float Lifetime { get; set; } = 2.0f;
     [Export] public bool AutoStart { get; set; } = true;
 
+    float timer;
+
     public override void _Ready() {
         Target ??= GetParent<Node>();
         if (AutoStart) Start(Lifetime);
     }
 
+    public override void _Process(double delta) {
+        if (timer > 0f) {
+            timer -= (float)delta;
+            if (timer <= 0f) {
+                OnTimeout();
+            }
+        }
+    }
+
     public void Start(float time) {
         if (time <= 0) return;
-        GetTree().CreateTimer(time).Timeout += OnTimeout;
+        timer = time;
     }
 
     private void OnTimeout() {
